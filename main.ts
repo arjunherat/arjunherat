b6e2d155139bd9268039d81badd20d75d192061e
arjunherat <arjunherat12@hotmail.com>
2026-01-26 14:44:40 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -7,26 +7,19 @@ input.onButtonPressed(Button.B, function () {
 let knuffel = 20
 let voeding = 20
 let dood = 0
+loops.everyInterval(500, function () {
+    voeding += -1
+    knuffel += -1
+})
 basic.forever(function () {
     if (knuffel > 10 && voeding > 10) {
         basic.showIcon(IconNames.Happy)
     }
-    if (knuffel < 10 && knuffel > 0) {
+    if (knuffel < 10 && knuffel > 0 || voeding < 10 && voeding > 0) {
         basic.showIcon(IconNames.Sad)
+        if (knuffel < 0 && voeding < 0) {
+            basic.showIcon(IconNames.Asleep)
+            dood += 1
+        }
     }
-    if (voeding < 10 && voeding > 0) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (knuffel < 0 && voeding < 0) {
-        basic.showIcon(IconNames.Asleep)
-        dood += 1
-    }
-    if (dood == 1) {
-        voeding = -900000
-        knuffel = -900000
-    }
-})
-loops.everyInterval(200, function () {
-    voeding += -1
-    knuffel += -1
 })
